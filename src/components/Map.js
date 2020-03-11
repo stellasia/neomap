dd0a66d86a0a4f740283133d6b73d0b5735e3305
Norbert Feron <norbert6feron@gmail.com>
2020-03-12 09:12:25 +1100

    remove react-leaflet

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-import {FeatureGroup, LayersControl, Map as LeafletMap, CircleMarker, Polyline, TileLayer, Popup} from 'react-leaflet'
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
-
+import L from 'leaflet';
 
 class Map extends Component {
 
@@ -11,155 +9,48 @@ class Map extends Component {
 		this.renderMarkerLayer = this.renderMarkerLayer.bind(this);
 		this.renderHeatmapLayer = this.renderHeatmapLayer.bind(this);
 		this.renderClusterLayer = this.renderClusterLayer.bind(this);
-		this.onFeatureGroupAdd = this.onFeatureGroupAdd.bind(this);
 	};
 
-
+	componentDidMount() {
+		// create map
+		this.map = L.map('map', {
+		  center: [49.8419, 24.0315],
+		  zoom: 16,
+		  layers: [
+			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}),
+		  ]
+		});
+	  }
 	renderMarker(d, j, color, opacity) {
 		/*Render maker with optional tooltip
          */
-		if (d.tooltip) {
-			return (
-				<CircleMarker key={j}
-							  center={d.pos}
-							  radius={5}
-							  color={color}
-							  fill={true}
-							  fillColor={color}
-							  fillOpacity={opacity}
-				>
-					<Popup>{d.tooltip}</Popup>
-				</CircleMarker>
-			)
-		}
-		return (
-			<CircleMarker key={j}
-						  center={d.pos}
-						  radius={5}
-						  color={color}
-						  fill={true}
-						  fillColor={color}
-						  fillOpacity={opacity}
-			>
-			</CircleMarker>
-		)
+		return null;
 	};
-
-
 	renderMarkerLayer(layer) {
 		/*Will show one marker per items in `layer.data`
          */
-		let data = layer.data;
-		let color = `rgb(${layer.color.r}, ${layer.color.g}, ${layer.color.b})`;
-		let opacity = layer.color.a;
-
-		return (
-			<LayersControl.Overlay key={layer.ukey} name={layer.name} checked>
-				<FeatureGroup onAdd={this.onFeatureGroupAdd}>
-					{
-						data.map((d, j) => {
-							return this.renderMarker(d, j, color, opacity);
-						})
-					}
-				</FeatureGroup>
-			</LayersControl.Overlay>
-		);
+		return null;
 	};
-
-
 	renderHeatmapLayer(layer) {
 		/* Create heatmap based on items in `layer.data`, each with weight 1.
          */
-		let data = layer.data;
-		return (
-			<LayersControl.Overlay key={layer.ukey} name={layer.name} checked>
-				<HeatmapLayer
-					fitBoundsOnLoad
-					points={data}
-					latitudeExtractor={m => m.pos[0]}
-					longitudeExtractor={m => m.pos[1]}
-					intensityExtractor={() => 1}
-					radius={layer.radius}
-					minOpacity={0.1}
-					max={10}
-				/>
-			</LayersControl.Overlay>
-		);
+		return null;
 	};
-
-
-	renderClusterLayer() {
-		/*TODO: cluster layer
+	renderClusterLayer(layer) {
+		/*Will show one marker per items in `layer.data`
          */
-		return "Cluster layer not supported for now";
+		return null;;
 	};
-
-
 	renderPolylineLayer(layer) {
 		/*Will show one marker per items in `layer.data`
          */
-		let data = layer.data;
-		let color = `rgb(${layer.color.r}, ${layer.color.g}, ${layer.color.b})`;
-		let positions = [];
-		data.forEach(el => {
-			positions.push(el.pos);
-		});
-		return (
-			<LayersControl.Overlay key={layer.ukey} name={layer.name} checked>
-				<FeatureGroup onAdd={this.onFeatureGroupAdd}>
-					<Polyline color={color} positions={positions} />
-				</FeatureGroup>
-			</LayersControl.Overlay>
-		);
+		return null;
 	};
-
-
-	onFeatureGroupAdd(e) {
-		if (e.target.getBounds()._northEast !== undefined)
-			this.refs.map.leafletElement.fitBounds(e.target.getBounds());
-	};
-
-
-	render() {
-		let layers = Object.entries(this.props.layers);
-
-		/*Map center will be the one of the last layer...
-           TODO: compute zoom or use leaflet tools to set it automatically?
-         */
-
-		let center = [47, 3];
-		let zoom = 4;
-
-		return (
-			<LeafletMap center={center} zoom={zoom} ref="map" preferCanvas={true} >
-				<LayersControl>
-					<LayersControl.BaseLayer name="Base" checked>
-						<TileLayer
-							attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-						/>
-					</LayersControl.BaseLayer>
-					{
-						layers.map(([, layer]) => {
-							if (layer.ukey !== undefined) {
-								if (layer.rendering === "polyline")
-									return this.renderPolylineLayer(layer);
-								if (layer.rendering === "markers")
-									return this.renderMarkerLayer(layer);
-								if (layer.rendering === "heatmap")
-									return this.renderHeatmapLayer(layer);
-								if (layer.rendering === "clusters")
-									return this.renderClusterLayer();
-								return "";
-							}
-							return "";
-						})
-					}
-				</LayersControl>
-			</LeafletMap>
-		)
+	render() {	
+		return <div id="map"></div>
 	}
 }
-
 
 export default  Map;
