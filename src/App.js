f4a9c4d11a703f7ddf127da686d32d5575ba42a7
jodom <jodomouma@gmail.com>
2020-08-21 23:24:01 -0700

    rewrite App as a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import neo4jService from './services/neo4jService'
 import download from "downloadjs";
 import { Map } from "./components/Map";
@@ -6,79 +6,67 @@ import { Menu } from "./components/Menu";
 import { SideBar } from "./components/SideBar";
 import "./App.css";
 
+export const App = React.memo(() => {
 
-export class App extends Component {
+	const [driver, setDriver] = React.useState(undefined);
+	const [layers, setLayers] = React.useState([]);
 
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			ready: false,
-			layers: []
-		};
-
-		this.saveConfigToFile = this.saveConfigToFile.bind(this);
-		this.loadConfigFromFile = this.loadConfigFromFile.bind(this);
-		this.addLayer = this.addLayer.bind(this);
-		this.updateLayer = this.updateLayer.bind(this);
-		this.removeLayer = this.removeLayer.bind(this);
-	};
-
-	getDriver() {
+	const getDriver = () => {
 		return neo4jService.getNeo4jDriver();
 	}
 
-	componentDidMount() {
-		this.getDriver().then( result => {
-			this.driver = result;
-		}).then( () => {
-			this.setState({
-				ready: true,
-			});
-		});
-	};
+	// This blocks render indefinitely if the driver is never resolved.
+	// A better pattern would be to render anyway without driver,
+	// only calling getDriver() when a module wants to use the driver.
+	// Error handle incase driver is not resolved at that point.
 
-	addLayer(layer) {
-		this.setState({ layers: [...this.state.layers, layer]});
+	// TODO: Remove boot blocker
+	React.useEffect(() => {
+		getDriver().then(result => {
+			setDriver(result);
+		});
+	});
+
+	const addLayer = (layer) => {
+		setLayers([...layers, layer]);
 	}
 
-	updateLayer(layer) {
-		const updatedLayers = this.state.layers.map(currentLayer => {
+	const updateLayer = (layer) => {
+		const updatedLayers = layers.map(currentLayer => {
 			if (currentLayer.key === layer.ukey) {
-				return layer; // OR possibly { ...currentLayer, ...layer }
+				return layer;
 			}
 			return currentLayer;
 		});
 
-		this.setState({ layers: updatedLayers });
+		setLayers(updatedLayers);
 	}
 
-	removeLayer(key) {
-		const filteredLayers = this.state.layers.filter(layer => layer.ukey !== key);
+	const removeLayer = (key) => {
+		const filteredLayers = layers.filter(layer => layer.ukey !== key);
 
-		this.setState({ layers: filteredLayers});
+		setLayers(filteredLayers);
 	}
 
-	saveConfigToFile(e) {
-		let config = JSON.stringify(this.props.layers);
-		let fileName = "neomap_config.json";
+	const saveConfigToFile = (e) => {
+		const config = JSON.stringify(layers);
+		const fileName = "neomap_config.json";
 		download(config, fileName, "application/json");
 		e.preventDefault();
 	};
 
-
-	loadConfigFromFile(e) {
+	const loadConfigFromFile = (e) => {
 		const fileSelector = document.createElement('input');
 		fileSelector.setAttribute('type', 'file');
 		fileSelector.click();
 		fileSelector.onchange = (ev) => {
 			const file = ev.target.files[0];
-			let fileReader = new FileReader();
+			const fileReader = new FileReader();
 			fileReader.onloadend = (e) => {
 				const content = e.target.result;
 				try {
 					const loadedLayers = JSON.parse(content);
-					this.setState({ layers: loadedLayers });
+					setLayers(loadedLayers);
 				} catch (err) {
 					// TODO: Build error UI
 					console.log('Failed to load and parse data from file', err);
@@ -89,37 +77,26 @@ export class App extends Component {
 		e.preventDefault();
 	};
 
-
-	renderUI() {
-		return (
+	return <React.Suspense fallback={<span>Loading...</span>}>
+		{driver && (
 			<div id="wrapper" className="row">
 				<div id="sidebar" className="col-md-4">
-					<Menu saveConfigToFile={this.saveConfigToFile} loadConfigFromFile={this.loadConfigFromFile} />
+					<Menu saveConfigToFile={saveConfigToFile} loadConfigFromFile={loadConfigFromFile} />
 					<SideBar
-						key="sidebar"
-						ref="sidebar"
-						driver = {this.driver}
-						layers={this.state.layers}
-						addLayer={this.addLayer}
-						updateLayer={this.updateLayer}
-						removeLayer={this.removeLayer}
+						driver = {driver}
+						layers={layers}
+						addLayer={addLayer}
+						updateLayer={updateLayer}
+						removeLayer={removeLayer}
 					/>
 				</div>
 				<div id="app-maparea" className="col-md-8">
 					<Map
 						key="map"
-						layers={this.state.layers}
+						layers={layers}
 					/>
 				</div>
 			</div>
-		);
-	};
-
-
-	render() {
-		// wait until driver is ready...
-		return this.state.ready ? this.renderUI() : (
-			<span>Loading...</span>
-		)
-	};
-}
+		)}
+	</React.Suspense>
+});
