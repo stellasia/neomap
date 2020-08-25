ec6563f3808bdc81e71e983af7690c7fbf0ae81b
jodom <jodomouma@gmail.com>
2020-08-25 01:19:38 -0700

    rewrite map component as function

--- src/components/Map.test.js
+++ src/components/Map.test.js
@@ -6,27 +6,27 @@ import { Map } from './Map';
 // TODO: Consider moving mock store setup to a util
 describe('Map tests', () => {
   it('renders the map', () => {
-    const map = render(<Map />).container;
+    const map = render(<Map layers={[]}/>).container;
     expect(map).toBeDefined();
   });
 
   it('plots marker layers', () => {
     // TODO: Mock marker layer and assert
-    render(<Map />);
+    render(<Map layers={[]}/>);
 
     expect(true); // FIXME
   });
 
   it('plots polyline layers', () => {
     // TODO: Mock polyline layer and assert
-    render(<Map />);
+    render(<Map layers={[]}/>);
 
     expect(true); // FIXME
   });
 
   it('plots heatmap layers', () => {
     // TODO: Mock heatmap layer and assert
-    render(<Map />);
+    render(<Map layers={[]}/>);
 
     expect(true); // FIXME
   });
