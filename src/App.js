53567ce63afc7c0c195837f770443bd6435c8d0a
nikita <nikita03565@gmail.com>
2021-09-30 00:37:10 +0300

    collapsing sidebar but there is a bug

--- src/App.js
+++ src/App.js
@@ -3,90 +3,101 @@ import download from "downloadjs";
 import { Map } from "./components/Map";
 import { Menu } from "./components/Menu";
 import { SideBar } from "./components/SideBar";
-import { neo4jService } from './services/neo4jService'
+import { neo4jService } from "./services/neo4jService";
 import "./App.css";
 
-
 export const App = React.memo(() => {
-	/**
-	 * Given the underlying neo4jDesktop drivers' dependency on global window context,
-	 * we need to import an instance here to the boot a service instance that reads
-	 * App.js window instance. The service is a singleton,
-	 * and subsequent windows will get the same instance with drivers created here.
-	 *
-	 * TODO: FIXME! Redesign neo4jService instantiation with full consideration for global window dependency
-	 */
-	neo4jService._getNeo4jDriver();
+  /**
+   * Given the underlying neo4jDesktop drivers' dependency on global window context,
+   * we need to import an instance here to the boot a service instance that reads
+   * App.js window instance. The service is a singleton,
+   * and subsequent windows will get the same instance with drivers created here.
+   *
+   * TODO: FIXME! Redesign neo4jService instantiation with full consideration for global window dependency
+   */
+  neo4jService._getNeo4jDriver();
 
-	const [layers, setLayers] = React.useState([]);
+  const [layers, setLayers] = React.useState([]);
+  const [collapsed, setCollapse] = React.useState(false);
 
-	const addLayer = (layer) => {
-		setLayers([...layers, layer]);
-	}
+  const addLayer = (layer) => {
+    setLayers([...layers, layer]);
+  };
 
-	const updateLayer = (layer) => {
-		const updatedLayers = layers.map(currentLayer => {
-			if (currentLayer.ukey === layer.ukey) {
-				return layer;
-			}
-			return currentLayer;
-		});
+  const updateLayer = (layer) => {
+    const updatedLayers = layers.map((currentLayer) => {
+      if (currentLayer.ukey === layer.ukey) {
+        return layer;
+      }
+      return currentLayer;
+    });
 
-		setLayers(updatedLayers);
-	}
+    setLayers(updatedLayers);
+  };
 
-	const removeLayer = (key) => {
-		const filteredLayers = layers.filter(layer => layer.ukey !== key);
+  const removeLayer = (key) => {
+    const filteredLayers = layers.filter((layer) => layer.ukey !== key);
 
-		setLayers(filteredLayers);
-	}
+    setLayers(filteredLayers);
+  };
 
-	const saveConfigToFile = (e) => {
-		const config = JSON.stringify(layers);
-		const fileName = "neomap_config.json";
-		download(config, fileName, "application/json");
-		e.preventDefault();
-	};
+  const saveConfigToFile = (e) => {
+    const config = JSON.stringify(layers);
+    const fileName = "neomap_config.json";
+    download(config, fileName, "application/json");
+    e.preventDefault();
+  };
 
-	const loadConfigFromFile = (e) => {
-		const fileSelector = document.createElement('input');
-		fileSelector.setAttribute('type', 'file');
-		fileSelector.click();
-		fileSelector.onchange = (ev) => {
-			const file = ev.target.files[0];
-			const fileReader = new FileReader();
-			fileReader.onloadend = (e) => {
-				const content = e.target.result;
-				try {
-					const loadedLayers = JSON.parse(content);
-					setLayers(loadedLayers);
-				} catch (err) {
-					// TODO: Build error UI
-					console.log('Failed to load and parse data from file', err);
-				}
-			};
-			fileReader.readAsText(file);
-		};
-		e.preventDefault();
-	};
+  const loadConfigFromFile = (e) => {
+    const fileSelector = document.createElement("input");
+    fileSelector.setAttribute("type", "file");
+    fileSelector.click();
+    fileSelector.onchange = (ev) => {
+      const file = ev.target.files[0];
+      const fileReader = new FileReader();
+      fileReader.onloadend = (e) => {
+        const content = e.target.result;
+        try {
+          const loadedLayers = JSON.parse(content);
+          setLayers(loadedLayers);
+        } catch (err) {
+          // TODO: Build error UI
+          console.log("Failed to load and parse data from file", err);
+        }
+      };
+      fileReader.readAsText(file);
+    };
+    e.preventDefault();
+  };
 
-	return (
-		<div id="wrapper" className="row">
-			<div id="sidebar" className="col-md-4">
-				<Menu saveConfigToFile={saveConfigToFile} loadConfigFromFile={loadConfigFromFile}/>
-				<SideBar
-					layers={layers}
-					addLayer={addLayer}
-					updateLayer={updateLayer}
-					removeLayer={removeLayer}
-				/>
-			</div>
-			<div id="app-maparea" className="col-md-8">
-				<Map
-					key="map"
-					layers={layers}
-				/>
-			</div>
-		</div>
-	)
+	const defaultMapOffset = 30;
+  const sidebarOffset = collapsed ? -27 : 0;
+  const mapOffset = defaultMapOffset + sidebarOffset;
+  const mapWidth = 100 - mapOffset;
+
+  return (
+    <div id="wrapper" className="row">
+      <div id="sidebar" className="col" style={{ left: `${sidebarOffset}%` }}>
+        <Menu
+          saveConfigToFile={saveConfigToFile}
+          loadConfigFromFile={loadConfigFromFile}
+          setCollapse={setCollapse}
+          collapsed={collapsed}
+        />
+        <SideBar
+          layers={layers}
+          addLayer={addLayer}
+          updateLayer={updateLayer}
+          removeLayer={removeLayer}
+        />
+      </div>
+      <div
+        id="app-maparea"
+        className="col"
+        style={{ left: `${mapOffset}%`, width: `${mapWidth}%` }}
+      >
+        <Map key="map" layers={layers} />
+      </div>
+    </div>
+  );
 });
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,38 +2,46 @@ import React from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import SettingsModal from "./SettingsModal";
 
-export const Menu = React.memo(({ saveConfigToFile, loadConfigFromFile }) => {
-  const [showModal, setShowModal] = React.useState(false);
+export const Menu = React.memo(
+  ({ saveConfigToFile, loadConfigFromFile, setCollapse, collapsed }) => {
+    const [showModal, setShowModal] = React.useState(false);
 
-  const handleClose = () => setShowModal(false);
-  const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
+    const toggleCollapse = () => setCollapse(!collapsed);
 
-  return (
-    <>
-      <Navbar>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="File" id="basic-nav-dropdown">
-              <NavDropdown.Item
-                href="#"
-                onClick={(e) => saveConfigToFile(e)}
-                className="beta"
-              >
-                Save As
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                href="#"
-                onClick={(e) => loadConfigFromFile(e)}
-                className="beta"
-              >
-                Open
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link onClick={handleShow}> Settings </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <SettingsModal handleClose={handleClose} show={showModal} />
-    </>
-  );
-});
+    return (
+      <>
+        <Navbar>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="File" id="basic-nav-dropdown">
+                <NavDropdown.Item
+                  href="#"
+                  onClick={(e) => saveConfigToFile(e)}
+                  className="beta"
+                >
+                  Save As
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="#"
+                  onClick={(e) => loadConfigFromFile(e)}
+                  className="beta"
+                >
+                  Open
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link onClick={handleShow}> Settings </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link onClick={toggleCollapse} style={{ marginLeft: "auto" }}>
+                {collapsed ? ">" : "<"}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <SettingsModal handleClose={handleClose} show={showModal} />
+      </>
+    );
+  }
+);
