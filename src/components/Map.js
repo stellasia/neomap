3f541ec3278cfe4e15a76733a821e467e999b7ed
nikita <nikita03565@gmail.com>
2021-10-09 15:16:38 +0300

    slight optimization

--- src/components/Map.js
+++ src/components/Map.js
@@ -136,14 +136,15 @@ export const Map = React.memo(({layers, sideBarCollapsed}) => {
 
 						// TODO: check if the layer has changed before rerendering it
 						relationsLayer.clearLayers();
-
+						
+						const showDirections = localStorage.getItem("show_directions") === "true";
 						data.forEach(entry => {
 							const polyline = L.polyline([entry.start, entry.end], {color: relRgbColor, opacity: relationshipColor.a}).addTo(relationsLayer)
 
 							if (entry.tooltip != null) {
 								polyline.bindPopup(entry.tooltip);
 							}
-							if (localStorage.getItem("show_directions") === "true") {
+							if (showDirections) {
 								const arrowheads = L.polylineDecorator(polyline, {
 									patterns: arrow,
 								})
